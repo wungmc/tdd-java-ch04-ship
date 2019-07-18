bd29e730ababb7381de34b8c601ba79890507727
王明初 <wangmingchu@wdai.com>
2019-07-18 18:20:37 +0800

    确定军舰位置和方向

--- src/main/java/com/wung/tddjava/ch04ship/Ship.java
+++ src/main/java/com/wung/tddjava/ch04ship/Ship.java
@@ -1,5 +1,16 @@
 package com.wung.tddjava.ch04ship;
 
 public class Ship {
-
+	
+	private Location location;
+	
+	public Ship(Location location) {
+		this.location = location;
+	}
+	
+	public Location getLocation() {
+		return location;
+	}
+	
+	
 }
