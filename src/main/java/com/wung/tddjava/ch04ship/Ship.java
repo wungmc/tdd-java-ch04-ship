d23f77d215b0b0391c9fab9fe11660bd97611764
王明初 <wangmingchu@wdai.com>
2019-07-18 17:12:50 +0800

    init

---
+++ src/main/java/com/wung/tddjava/ch04ship/Ship.java
@@ -0,0 +1,5 @@
+package com.wung.tddjava.ch04ship;
+
+public class Ship {
+
+}
---
+++ src/test/java/com/wung/tddjava/ch04ship/ShipSpec.java
@@ -0,0 +1,8 @@
+package com.wung.tddjava.ch04ship;
+
+import org.testng.annotations.*;
+
+@Test
+public class ShipSpec {
+
+}
