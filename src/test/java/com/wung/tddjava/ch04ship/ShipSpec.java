60256802f426ddfc1e683ec12e7468542014d56c
wungmc <wungcc@gmail.com>
2019-07-18 18:20:37 +0800

    确定军舰位置和方向

--- src/test/java/com/wung/tddjava/ch04ship/ShipSpec.java
+++ src/test/java/com/wung/tddjava/ch04ship/ShipSpec.java
@@ -1,8 +1,33 @@
 package com.wung.tddjava.ch04ship;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 
+
+/**
+ * TestNG 与 JUnit 不同之处：
+ * 1. TestNG 的 @Test 可以加在类上，表示该类的所有方法都是测试；
+ * 2. 测试类运行时，TestNG 使用同一个测试实例，而 JUnit 的每个测试都是不同的实例。
+ *
+ */
 @Test
 public class ShipSpec {
-
+	
+	private int x = 10;
+	private int y = 20;
+	private Location location;
+	private Ship ship;
+	
+	
+	@BeforeMethod
+	public void beforeMethod() {
+		Direction direction = Direction.NORTH;
+		location = new Location(new Point(x, y), direction);
+		ship = new Ship(location);
+	}
+	
+	public void whenInstantiatedThenLocationIsSet() {
+		Assert.assertEquals(ship.getLocation(), location);
+	}
+	
 }
